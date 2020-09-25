43df736c03d5483535b7e3c8c5f51206b4e613f4
pogudingleb <pogudin.gleb@gmail.com>
2020-09-25 15:02:53 +0300

    macros in jl-examples

--- examples/SEIR.jl
+++ examples/SEIR.jl
@@ -1,16 +1,14 @@
 using BenchmarkTools
 include("../experiments_bounds.jl")
 
-varnames = ["S", "E", "I", "N", "c", "a", "b", "d", "nu"]
-R, (S, E, I, N, c, a, b, d, nu) = PolynomialRing(QQ, varnames)
-
-ode = ODE(Dict(
-    S => -b * S * I // N,
-    E => b * S * I // N - nu * E,
-    I => nu * E - a * I,
-    N => R(0),
-    c => R(0)
-), [])
+ode = @ODEmodel(
+    S'(t) = -b * S(t) * I(t) / N(t),
+    E'(t) = b * S(t) * I(t) / N(t) - nu * E(t),
+    I'(t) = nu * E(t) - a * I(t),
+    N'(t) = 0,
+    c'(t) = 0,
+    [d]
+)
 
 # compiles the function and runs a number of times, outputs the mean runtime
 @btime bound_number_experiments(ode, [c * I + d * E, c, N])
--- examples/Section5.jl
+++ examples/Section5.jl
@@ -1,12 +1,10 @@
 using BenchmarkTools
 include("../experiments_bounds.jl")
 
-R, (x1, x2, mu1, mu2) = PolynomialRing(QQ, ["x1", "x2", "mu1", "mu2"])
-
-ode = ODE(Dict(
-    x1 => R(0),
-    x2 => x1 * x2 + mu1 * x1 + mu2
-), [])
+ode = @ODEmodel(
+    x1'(t) = 0,
+    x2'(t) = x1(t) * x2(t) + mu1 * x1(t) + mu2
+)
 
 # compiles the function and runs a number of times, outputs the mean runtime
 @btime bound_number_experiments(ode, [x2])
