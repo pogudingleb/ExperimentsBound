aa6512525111d6413455af27ffbb3baf7e269298
pogudingleb <pogudin.gleb@gmail.com>
2020-09-29 10:13:09 +0300

    lincomp example improved

--- examples/linear_compartment.jl
+++ examples/linear_compartment.jl
@@ -1,14 +1,13 @@
-function linear_compartment_model(graph, sinks, inputs)
+function linear_compartment_model(graph, sinks)
     """
     Input: 
         - graph - graph of the model network represented via adjacency lists
         - sinks - the indices of nodes having a sink
-        - inputs - list of the nodes having differentially transcendental input
     Output: the corresponding ODE object where each parameter a_ij is replaced
-            with a_ij + b_ij * v, where v is a constant input encoded as a constant output
+            with a_ij + b_ij * x_0, where x_0 is a constant input encoded as a constant output
     """
     n = length(graph)
-    x_vars_names = ["x$i" for i in 1:n]
+    x_vars_names = ["x$i" for i in 0:n]
     edges_vars_names = Array{String, 1}()
     for i in 1:n
         for j in graph[i]
@@ -20,15 +19,15 @@ function linear_compartment_model(graph, sinks, inputs)
         push!(edges_vars_names, "a_$(s)_0")
         push!(edges_vars_names, "b_$(s)_0")
     end
-    u_vars_names = ["u$i" for i in inputs]
-    R, vars = PolynomialRing(QQ, vcat(x_vars_names, edges_vars_names, u_vars_names, ["v"]))
-    x_vars = vars[1:n]
-    v = vars[end]
-    equations = Dict(x => R(0) for x in x_vars)
-    equations[v] = R(0)
+    R, vars = PolynomialRing(QQ, vcat(x_vars_names, edges_vars_names))
+    x_vars = vars[2:(n + 1)]
+    x0 = vars[1]
+    equations = Dict{fmpq_mpoly, Union{fmpq_mpoly, Generic.Frac{fmpq_mpoly}}}(x => R(0) for x in x_vars)
+    equations[x0] = R(0)
     for i in 1:n
         for j in graph[i]
-            rate = str_to_var("a_$(i)_$(j)", R) + str_to_var("b_$(i)_$(j)", R) * v
+            rate = str_to_var("a_$(i)_$(j)", R) + str_to_var("b_$(i)_$(j)", R) * x0
+
             if i != j
                 equations[x_vars[j]] += x_vars[i] * rate
                 equations[x_vars[i]] -= x_vars[i] * rate
@@ -37,14 +36,11 @@ function linear_compartment_model(graph, sinks, inputs)
             end
         end
         if i in sinks
-            rate = str_to_var("a_$(i)_0", R) + str_to_var("b_$(i)_0", R) * v
+            rate = str_to_var("a_$(i)_0", R) + str_to_var("b_$(i)_0", R) * x0
             equations[x_vars[i]] += -x_vars[i] * rate
         end
-        if i in inputs
-            equations[x_vars[i]] += str_to_var("u$i", R)
-        end
     end
-    return ODE(equations, [str_to_var("u$i", R) for i in inputs])
+    return ODE{fmpq_mpoly}(equations)
 end
 
 #------------------------------------------------------------------------------
